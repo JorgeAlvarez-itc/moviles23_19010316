32006a7b91ae45b9b533c839da4ce94d7311b6b2
Jorge <19030136@itcelaya.edu.mx>
2023-02-25 15:57:47 -0600

    Commit con cambios practica2 Login responsive

--- prueba1/lib/screens/login_screen.dart
+++ prueba1/lib/screens/login_screen.dart
@@ -1,123 +1,209 @@
+import '../responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:prueba1/widgets/loading_modal_widget.dart';
 import 'package:social_login_buttons/social_login_buttons.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isLoading = false;
 
-  bool isLoading=false;
-  
   final txtEmail = TextFormField(
     decoration: const InputDecoration(
-      label: Text("EMAIL USER"),
-      border: OutlineInputBorder()
-    ),
+        labelText: "EMAIL USER", border: OutlineInputBorder()),
   );
-
 
   final txtPass = TextFormField(
-    decoration:const InputDecoration(
-      label: Text("Password"),
-      border: OutlineInputBorder()
-    ),
+    decoration: const InputDecoration(
+        labelText: "Password", border: OutlineInputBorder()),
   );
-  
-  final horizontalSpace = SizedBox(height: 10,);
-  
+
+  final horizontalSpace = SizedBox(
+    height: 10,
+  );
+  final verticalSpace = SizedBox(
+    width: 10,
+  );
   final googlebtn = SocialLoginButton(
-    buttonType: SocialLoginButtonType.google, 
-    onPressed: (){},
+    buttonType: SocialLoginButtonType.google,
+    onPressed: () {},
   );
   final facebookbtn = SocialLoginButton(
-    buttonType: SocialLoginButtonType.facebook, 
-    onPressed: (){},
+    buttonType: SocialLoginButtonType.facebook,
+    onPressed: () {},
   );
   final gitbtn = SocialLoginButton(
-    buttonType: SocialLoginButtonType.github, 
-    onPressed: (){},
+    buttonType: SocialLoginButtonType.github,
+    onPressed: () {},
   );
-  final imgLogo = Image.asset("assets/logo.png", height: 200,);
+  final imgLogo = Image.asset(
+    "assets/logo.png",
+    height: 120,
+  );
 
   @override
   Widget build(BuildContext context) {
-    final buttonlogging = SocialLoginButton(
-    buttonType: SocialLoginButtonType.generalLogin, 
-    onPressed: (){
-      isLoading=true;
-      setState(() {
-        
-      });
-      Future.delayed(const Duration(milliseconds: 4000)).then((value){
-        isLoading=false;
-        Navigator.pushNamed(context, '/dash');
-        setState(() {});
-      });
-    },
-    );
-    final txtRegister =Padding(
-    padding: const EdgeInsets.all(1), 
-    child: TextButton(
-      onPressed: () {
-        Navigator.pushNamed(context, '/register');
-      },
-      child:const Text('Crear cuenta',
-      style:TextStyle(
-        fontSize: 18,
-        decoration: TextDecoration.underline
-      ) ,)
-  ));  
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Container(
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                opacity: .3,
-                fit: BoxFit.cover,
-                image: AssetImage('assets/fondo.png')
-              )
-            ),
+                image: DecorationImage(
+                    opacity: .3,
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/fondo.png'))),
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      horizontalSpace,
-                      txtEmail,
-                      horizontalSpace,
-                      txtPass,
-                      horizontalSpace,
-                      buttonlogging,
-                      horizontalSpace,
-                      googlebtn,
-                      horizontalSpace,
-                      facebookbtn,
-                      horizontalSpace,
-                      gitbtn,
-                      horizontalSpace,
-                      txtRegister
-                    ],
-                  ),
-                  Positioned( 
-                  top: 130,
-                  child:imgLogo)
-                ],
+              child: Responsive(
+                mobile: _buildMobileContent(),
+                tablet: _buildTabletContent(),
+                desktop: _buildDesktopContent(),
               ),
             ),
           ),
-          isLoading?const LoadingModalWidget():Container()
+          isLoading ? const LoadingModalWidget() : Container()
         ],
       ),
+    );
+  }
+
+  Widget _buildMobileContent() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        horizontalSpace,
+        FractionallySizedBox(
+          widthFactor:
+              0.8, // ajusta el ancho de la imagen al 80% del ancho de la pantalla
+          child: imgLogo,
+        ),
+        horizontalSpace,
+        txtEmail,
+        horizontalSpace,
+        txtPass,
+        horizontalSpace,
+        _buildLoginButton(),
+        horizontalSpace,
+        googlebtn,
+        horizontalSpace,
+        facebookbtn,
+        horizontalSpace,
+        gitbtn,
+        horizontalSpace,
+        _buildRegisterTextButton(),
+      ],
+    );
+  }
+
+  Widget _buildTabletContent() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              FractionallySizedBox(
+                widthFactor: 0.8, // ajusta el ancho de la imagen al 80% del ancho de la pantalla
+                child: imgLogo,
+              ),
+              txtEmail,
+              horizontalSpace,
+              txtPass,
+              horizontalSpace,
+              _buildLoginButton(),
+              horizontalSpace,
+              _buildRegisterTextButton(),
+            ],
+          ),
+        ),
+        verticalSpace,
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              googlebtn,
+              horizontalSpace,
+              facebookbtn,
+              horizontalSpace,
+              gitbtn,
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDesktopContent() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              imgLogo,
+              horizontalSpace,
+              txtEmail,
+              horizontalSpace,
+              txtPass,
+              horizontalSpace,
+              _buildLoginButton(),
+              horizontalSpace,
+              _buildRegisterTextButton(),
+            ],
+          ),
+        ),
+        verticalSpace,
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              googlebtn,
+              horizontalSpace,
+              facebookbtn,
+              horizontalSpace,
+              gitbtn,
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return SocialLoginButton(
+      buttonType: SocialLoginButtonType.generalLogin,
+      onPressed: () {
+        isLoading = true;
+        setState(() {});
+        Future.delayed(const Duration(milliseconds: 4000)).then((value) {
+          isLoading = false;
+          Navigator.pushNamed(context, '/dash');
+          setState(() {});
+        });
+      },
+    );
+  }
+
+  Widget _buildRegisterTextButton() {
+    return TextButton(
+      onPressed: () {
+        Navigator.pushNamed(context, '/register');
+      },
+      child: const Text('Don\'t have an account? Sign up'),
     );
   }
 }
